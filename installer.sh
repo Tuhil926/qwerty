7ca9330849b31f4e5c038cc4edd5adc5d3082632
Tuhil <Tuhil>
2025-05-11 23:33:03 +0530

    made an installer script

---
+++ installer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$EUID" -eq 0 ]
+  then echo "Please don't run as root"
+  exit
+fi
+
+pip install pygame
+pip install pyperclip
+pip install pycryptodome
+
+mkdir -p ~/.config/qwerty
+cp qwerty.py ~/.config/qwerty
+cp qwerty.png ~/.config/qwerty
+cp PixelOperator8.ttf ~/.config/qwerty
+chmod +x qwerty
+sudo cp qwerty /usr/bin
+
+echo "qwerty installed successfully! run the command 'qwerty' to start it.\n"
+echo "If this is your first time installing the app, the password is 'qwerty'. You can change it by pressing CTRL+P.\n"
+echo "read the given README for further details on how to use.\n"
